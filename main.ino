0f066bdce2e0b479162c7c20fed78aec11e92b23
jecndlria <jecndlria@gmail.com>
2022-05-20 22:49:49 -0700

    Successful test on first complexity (shift register)

--- main.ino
+++ main.ino
@@ -1,10 +1,43 @@
+#include "SevSegShift.h"
+
+#define SHIFT_PIN_DS   13 /* Data input PIN */
+#define SHIFT_PIN_STCP 12 /* Shift Register Storage PIN */
+#define SHIFT_PIN_SHCP 11 /* Shift Register Shift PIN */
+int test = 0; 
+int delayManual = 0;
+
+SevSegShift sevseg(SHIFT_PIN_DS, SHIFT_PIN_SHCP, SHIFT_PIN_STCP); //Instantiate a seven segment controller object (with Shift Register functionality)
+
 void setup() {
   // put your setup code here, to run once:
+  byte numDigits = 4;
+  byte digitPins[] = {8+2, 8+5, 8+6, 2}; // of ShiftRegister(s) | 8+x (2nd Register)
+  byte segmentPins[] = {8+3, 8+7, 4, 6, 7, 8+4, 3,  5}; // of Shiftregister(s) | 8+x (2nd Register)
+  /* configuration without ShiftRegister - Direct arduino connection
+  byte digitPins[] = {2, 3, 4, 5}; // PINs of Arduino
+  byte segmentPins[] = {6, 7, 8, 9, 10, 11, 12, 13}; // PINs of Arduino
+  */
+  bool resistorsOnSegments = true; // 'false' means resistors are on digit pins
+  byte hardwareConfig = COMMON_CATHODE; // See README.md for options
+  bool updateWithDelays = false; // Default 'false' is Recommended
+  bool leadingZeros = false; // Use 'true' if you'd like to keep the leading zeros
+  bool disableDecPoint = false; // Use 'true' if your decimal point doesn't exist or isn't connected. Then, you only need to specify 7 segmentPins[]
 
+  sevseg.begin(hardwareConfig, numDigits, digitPins, segmentPins, resistorsOnSegments,
+  updateWithDelays, leadingZeros, disableDecPoint);
+  test = 0;
+  delayManual = 0;
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-    Serial.print("hi");
-    delay(1000);
+  sevseg.refreshDisplay();
+
+  if (delayManual == 10000) {
+    sevseg.setNumber(test++);
+    delayManual = 0;
+  }
+  else {
+    delayManual++;
+  }
 }
