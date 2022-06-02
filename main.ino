802c06c49f03305b09d6bf47b23592459f77a7db
jecndlria <jecndlria@gmail.com>
2022-06-01 22:04:12 -0700

    added boilerplate code for states

--- main.ino
+++ main.ino
@@ -1,16 +1,7 @@
-//void initializeSevenSegmentDisplay();   // DEFINED IN shiftregister_4digit7seg.ino
-//void sevenSegmentTimerTest();           // DEFINED IN shiftregister_4digit7seg.ino
-//void initializeScreen();                // DEFINED IN lcd.ino
-//void testScreen();                      // DEFINED IN lcd.ino
-//void initializeGestureSensor();         // DEFINED IN gesture.ino
-//void testGestureSensor();               // DEFINED IN gesture.ino
-//void initializeGame();                  // DEFINED IN game.ino
-//void gameBoardDebug();                  // DEFINED IN game.ino
 #include "headers/button.h"
 #include "headers/eeprom.h"
 #include "headers/game.h"
 #include "headers/gesture.h"
-#include "headers/joystick.h"
 #include "headers/lcd.h"
 #include "headers/shiftregister_4digit7seg.h"
 
@@ -25,6 +16,36 @@ typedef struct task {
 const unsigned short tasksNum = 2;
 task tasks[tasksNum];
 
+#define SAVEBUTTON 10
+#define RESETBUTTON 9
+#define SCOREBUTTON 8
+
+enum Button_States{BUTTON_INIT};
+int buttonTick(int state)
+{
+    switch(state)
+    {
+
+    }
+    switch(state)
+    {
+
+    }
+}
+
+enum Game_States{GAME_INIT};
+int gameTick(int state)
+{
+    switch(state)
+    {
+
+    }
+    switch(state)
+    {
+
+    }
+}
+
 void setup()
 {
     Serial.begin(9600);
@@ -33,42 +54,41 @@ void setup()
     initializeSevenSegmentDisplay();   
     initializeScreen();
     initializeGestureSensor();
-    // BRING BACK LATER! initializeGame();
-    gameBoardDebug();
-    drawBoard();
-    pinMode(8, INPUT);
-    pinMode(9, INPUT);
-    pinMode(10, INPUT);
+    //initializeGame();
+    pinMode(SCOREBUTTON, INPUT);
+    pinMode(RESETBUTTON, INPUT);
+    pinMode(SAVEBUTTON, INPUT);
     //storeGameBoard();
     loadGameBoard();
+    gameBoardDebug();
     drawBoard();
-    /*
-    tasks[i].state = INIT0;
-    tasks[i].period = 500;
+
+    unsigned char i = 0;
+    tasks[i].state = GAME_INIT;
+    tasks[i].period = 50;
     tasks[i].elapsedTime = 0;
-    tasks[i].TickFct = &SM1_Tick;
+    tasks[i].TickFct = &gameTick;
     i++;
-    tasks[i].state = INIT1;
-    tasks[i].period = 100;
+    tasks[i].state = BUTTON_INIT;
+    tasks[i].period = 50;
     tasks[i].elapsedTime = 0;
-    tasks[i].TickFct = &SM2_Tick;
-    */
+    tasks[i].TickFct = &buttonTick;
 
 }
 void loop()
 {
-    /*
- 
     unsigned char i;
     for (i = 0; i < tasksNum; ++i) {
         if ( (millis() - tasks[i].elapsedTime) >= tasks[i].period) {
             tasks[i].state = tasks[i].TickFct(tasks[i].state);
             tasks[i].elapsedTime = millis(); // Last time this task was ran
-        
         }
     }
-    */
+   
+    //TEST CODE
+    /*
     if (digitalRead(8) == LOW) { checkMove(); sevseg.blank(); if (gameWon) { printWin(); delay(10000);}}
     else { sevseg.setNumber(score); sevseg.refreshDisplay();}
     if (digitalRead(9) == HIGH) {resetGame(); initializeGame(); drawBoard(); score = 0;}
+    */
 }
